49361be87877a89fbc8f16f94987758ea9ba3d0e
John Doe <johndoe@email.com>
2023-04-27 18:51:47 -0400

    added react for user design page

---
+++ SelfDesignProfile/my-app/src/components/CustomizationOptions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CustomizationOptions extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Your JSX code for customization options will go here */}
+      </div>
+    );
+  }
+}
+
+export default CustomizationOptions;
