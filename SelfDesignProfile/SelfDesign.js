c1e8e00b4c580db76e85abc460a3a115bf6d5f79
John Doe <johndoe@email.com>
2023-04-27 18:52:21 -0400

    commit

--- SelfDesignProfile/SelfDesign.js
+++ SelfDesignProfile/SelfDesign.js
@@ -19,8 +19,11 @@ $(document).ready(function() {
   $('#add-textbox').on('click', function() {
     textboxCount++;
     const newTextbox = $(`<div id="textbox-${textboxCount}" class="draggable resizable">
-                        <textarea style="background-color: transparent; border: none; outline: none;"></textarea>
-                      </div>`);
+                    <textarea style="background-color: transparent; border: none; outline: none;"></textarea>
+                  </div>`);
+      const elementContainer = $('<div class="element-container"></div>');
+      elementContainer.append(newTextbox);
+      $('.canvas').append(elementContainer);
 
     $('.textboxes-container').append(newTextbox);
     newTextbox.draggable().resizable();
@@ -30,8 +33,10 @@ $(document).ready(function() {
     newTextbox.addClass('selected');
     currentTextbox = newTextbox;
 
-    // Scroll to the newly added textbox
-    $('.textboxes-container').animate({ scrollTop: $('.textboxes-container')[0].scrollHeight }, 1000);
+    // Wait for the new textbox to be created before selecting it
+    newTextbox.ready(function() {
+      newTextbox.find('textarea').trigger('click');
+    });
   });
 
   // Set the font size for the currently selected textbox
@@ -42,20 +47,30 @@ $(document).ready(function() {
     }
   });
 
- // Highlight the new textbox when a textbox is clicked
- $(document).on('click', '.resizable', function() {
-  $('.resizable').removeClass('selected');
-  $(this).addClass('selected');
-  currentTextbox = $(this).find('textarea');
-});
-
-  // Deselect the currentTextbox when clicking outside of textboxes
+  // Select the clicked textbox
+  $(document).on('click', '.resizable', function() {
+    $('.resizable').removeClass('selected');
+    $(this).addClass('selected');
+    currentTextbox = $(this);
+  });
+  //deselects hightlight
   $(document).on('click', function(event) {
-    if (!$(event.target).closest('.resizable').length) {
+    if (!$(event.target).closest('.resizable').length && !$(event.target).closest('.customization-options').length) {
       $('.resizable').removeClass('selected');
       currentTextbox = null;
     }
   });
+  //text box deleter
+  $(document).on('keydown', function(e) {
+    if (e.which === 8) { // 8 is the key code for the delete key
+      if (currentTextbox && !$(document.activeElement).is('textarea')) {
+        currentTextbox.remove(); // remove the selected text box
+        currentTextbox = null;
+      }
+    }
+  });
+  
+
 });
 
 
@@ -76,6 +91,45 @@ $(document).ready(function() {
 $('#create-box').on('click', function() {
   let color = $('#color-picker').val();
   let $coloredBox = $('<div class="color-box"></div>').css('background-color', color);
+    const elementContainer = $('<div class="element-container"></div>');
+    elementContainer.append($coloredBox);
+  $('.canvas').append(elementContainer);
   $('.textboxes-container').append($coloredBox);
   $coloredBox.draggable().resizable();
 });
+
+
+
+// image uploader before
+
+$('#add-image').on('click', function() {
+  $('#image-input').click();
+});
+
+$('#image-input').on('change', function(event) {
+  const file = event.target.files[0];
+  if (file && file.type.match('image.*')) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const newImage = $(`<img src="${e.target.result}">`);
+      const imageContainer = $(`<div class="draggable resizable"></div>`);
+      imageContainer.append(newImage);
+      elementContainer.append(imageContainer);
+      $('.canvas').append(elementContainer);
+      $('.textboxes-container').append(imageContainer);
+      imageContainer.draggable().resizable({ aspectRatio: true });
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+
+// For the text boxes
+$('.canvas').append(newTextbox);
+
+// For the color boxes
+$('.canvas').append($coloredBox);
+
+// For the image uploader
+$('.canvas').append(imageContainer);
+
---
+++ SelfDesignProfile/my-app/src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainContent extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Your JSX code for main content will go here */}
+      </div>
+    );
+  }
+}
+
+export default MainContent;
