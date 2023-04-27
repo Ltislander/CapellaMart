62be10277318160d28d0446ba96fb431e272690c
John Doe <johndoe@email.com>
2023-04-27 14:44:32 -0400

    half working

--- SelfDesignProfile/SelfDesign.js
+++ SelfDesignProfile/SelfDesign.js
@@ -14,17 +14,51 @@ document.getElementById('font-size').addEventListener('change', function() {
 
 $(document).ready(function() {
   let textboxCount = 0;
+  let currentTextbox = null;
 
   $('#add-textbox').on('click', function() {
     textboxCount++;
     const newTextbox = $(`<div id="textbox-${textboxCount}" class="draggable resizable">
-                            <textarea></textarea>
-                          </div>`);
+                        <textarea style="background-color: transparent; border: none; outline: none;"></textarea>
+                      </div>`);
 
     $('.textboxes-container').append(newTextbox);
     newTextbox.draggable().resizable();
+
+    // Deselect all other textboxes when a new one is created
+    $('.resizable').removeClass('selected');
+    newTextbox.addClass('selected');
+    currentTextbox = newTextbox;
+
+    // Scroll to the newly added textbox
+    $('.textboxes-container').animate({ scrollTop: $('.textboxes-container')[0].scrollHeight }, 1000);
+  });
+
+  // Set the font size for the currently selected textbox
+  $('#fontsize').on('input', function() {
+    const fontSize = $(this).val();
+    if (currentTextbox) {
+      currentTextbox.find('textarea').css('font-size', fontSize + 'px');
+    }
+  });
+
+ // Highlight the new textbox when a textbox is clicked
+ $(document).on('click', '.resizable', function() {
+  $('.resizable').removeClass('selected');
+  $(this).addClass('selected');
+  currentTextbox = $(this).find('textarea');
+});
+
+  // Deselect the currentTextbox when clicking outside of textboxes
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.resizable').length) {
+      $('.resizable').removeClass('selected');
+      currentTextbox = null;
+    }
   });
 });
+
+
 
 $(document).ready(function() {
   $('#create-box').on('click', function() {
